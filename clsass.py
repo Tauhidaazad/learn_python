46b691cc6ed92f16d014a2b7de78f8f9479981e2
tauhid azad <tauhidazad.ta@gmail.com>
2019-07-28 08:59:48 +0545

    class.py

---
+++ clsass.py
@@ -0,0 +1,33 @@
+# for i in range(2,51,2):
+#     print(i)
+
+# for i in range(50, 0, -2):
+#     print(i)
+
+# for i in range(1,5):
+#     print("*" * i)
+ 
+# for i in range(1,6):
+#     for i in range(1,i+1):
+#         print("*", end**)
+
+# for i in range(5, 0,-1):
+#    for j in range(5, i+1, -1):
+#        print("*", end='')
+#     print(0
+
+# fac = 0
+# num = int(input("number"))
+
+# while(num >= 1):
+
+#    fac = num + fac
+#    num = num-1
+# print(fac)
+
+for latter in "hello world":
+   print(latter)
+
+for latter in "Tauhid Azad":
+   print(latter)
+for latter in "j"
